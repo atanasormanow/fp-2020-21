1616b1af70549fbe9be655cfc3cbbef8b5d87c92
Atanas <atanasormanow@gmail.com>
2022-06-12 16:48:04 +0300

    Delete all except group-d

--- group-a/Week13/ListOp.hs
+++
@@ -1,7 +0,0 @@
-module ListOp
-  ( map'
-  ) where
-
-map' :: (a -> b) -> [a] -> [b]
-map' f [] = []
-map' f (head:tail) = (f head) : map' f tail
--- group-f/README.md
+++
@@ -1,3 +0,0 @@
-# Упражнения група F
-
-Провеждане: **Google Meet**: [https://meet.google.com/yzu-fzea-uvp](https://meet.google.com/yzu-fzea-uvp)
